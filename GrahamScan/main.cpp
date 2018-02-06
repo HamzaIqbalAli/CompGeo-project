6420c358b7d320ead61b844ef5cd4891d714ab1d
Jezza49 <hamzaiqbalali49@gmail.com>
2018-02-06 19:06:06 +0530

    graham working

--- GrahamScan/main.cpp
+++ GrahamScan/main.cpp
@@ -6,18 +6,23 @@
 using namespace std;
 vector <pair<float,float> > points;
 int num_of_points,left_most;
+bool isColl(int a,int b, int c)
+{
+    float s1,s2;
+    s1 = (points[b].second - points[a].second) * (points[b].first - points[a].first);
+    s2 = (points[c].second - points[a].second) * (points[c].first - points[a].first);
+    if(s1==s2)  return 0;
+    else    return 1;
+}
 
 int findOrientation(int p1,int p2,int p3)
 {
-    float s1,s2,dist1,dist2;
-    s1 = (points[p2].second - points[p1].second) * (points[p3].first - points[p2].first);
-    s2 = (points[p2].first - points[p1].first) * (points[p3].second - points[p2].second);
-    if(s1 == s2)
-    {
-        return 0;
-    }
-    return (s1>s2) ? 1 : 2;
-
+    float s1,s2;
+    s1 = (points[p3].second - points[p2].second) * (points[p2].first - points[p1].first);
+    s2 = (points[p2].second - points[p1].second) * (points[p3].first - points[p2].first);
+    if(s1==s2)  return -1;
+    else   if(s1>s2)    return 1;
+    else return -1;
 }
 
 int firstpoint(stack <int> &s)
@@ -31,17 +36,20 @@ int firstpoint(stack <int> &s)
 
 int compareFunction(const pair<float,float> &p1, const pair<float,float> &p2)
 {
-    float s1,s2,dist1,dist2;
-    s1 = (p1.second - points[left_most].second) * (p2.first - p1.first);
-    s2 = (p1.first - points[left_most].first) * (p2.second - p1.second);
-    if(s1 == s2)
-    {
-        dist1 = (abs(points[left_most].first - p1.first)+abs(points[left_most].second - p1.second));
-        dist2 = (abs(points[left_most].first - p2.first)+abs(points[left_most].second - p2.second));
-        return (dist2>=dist1) ? -1 : 1 ;
-    }
+    float s1,s2;
+    s1 = (p2.second-p1.second) * (p1.first-points[left_most].first);
+    s2 = (p1.second-points[left_most].second) * (p2.first-p1.first);
+    if(s1>s2)   return 1;
+    else if(s1<s2)  return 0;
     else
-        return (s2>s1) ? -1 : 1;
+    {
+        float dist1 = abs(points[left_most].first - p1.first) + abs(points[left_most].second - p1.second);
+        float dist2 = abs(points[left_most].first - p2.first) + abs(points[left_most].second - p2.second);
+        if(dist1<dist2) return 1;
+        else return 0;
+    }
+
+
 }
 
 void swapPoints(int lleft_most)
@@ -58,34 +66,36 @@ void GrahamScan()
     left_most = 0;int i;
     for(i=1;i<num_of_points;i++)
     {
-        if((points[i].second<points[left_most].second) || (points[i].second==points[left_most].second && points[i].first<points[left_most].first))
+        if((points[i].first<points[left_most].first) || (points[i].first==points[left_most].first && points[i].second<points[left_most].second))
             left_most=i;
     }
     swapPoints(left_most);
     left_most=0;
     sort(points.begin()+1,points.end(),compareFunction);
-    vector <int> newPoints;
+    int newn = 1;
     for(i=1;i<num_of_points;i++)
     {
-        while(i<num_of_points-1 && findOrientation(left_most,i,i+1)==0)
+        while(i<num_of_points-1 && isColl(left_most,i,i+1)==0)
+        {
             i++;
-        newPoints.push_back(i);
+        }
+        points[newn]=points[i];
+        newn++;
     }
-    int sz = newPoints.size();
-    if(sz<3)    return;
-
+    //------------------*/
+    //if(sz<3)    return
     stack <int> s;
     s.push(left_most);
     s.push(1);
     s.push(2);
 
-    for(i=3;i<sz;i++)
+    for(i=3;i<newn;i++)
     {
-        while(findOrientation(firstpoint(s),s.top(),i)!=2)
+        while(findOrientation(firstpoint(s),s.top(),i)!=1)
             s.pop();
         s.push(i);
     }
-    cout<<"NUmber of points in hull - "<<s.size()<<"\n";
+    cout<<"Number of points in hull - "<<s.size()<<"\n";
     while(s.empty()==false)
     {
         int j = s.top();
